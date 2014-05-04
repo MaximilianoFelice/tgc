c90c3b8fd6c215310e2e89915ff2201ce891accf
Julian Noziglia <julian.noziglia@gmail.com>
2014-05-04 20:44:27 -0300

    Creada una isla y palmeras

--- AlumnoEjemplos/SeaSharp/EjemploAlumno.cs
+++ AlumnoEjemplos/SeaSharp/EjemploAlumno.cs
@@ -69,6 +69,9 @@ namespace AlumnoEjemplos.SeaSharp
             ship.Load();
             EnemyFleet.AddEnemy();
 
+            /* Cargamos la isla */
+            Island.Load();
+
 
             #region (Otras Camaras)
             /*
@@ -127,7 +130,8 @@ namespace AlumnoEjemplos.SeaSharp
             ship.Render();
             EnemyFleet.RenderAll();
             Bola.RenderAll();
-
+            Island.Render();
+            
            
         }
 
@@ -141,6 +145,7 @@ namespace AlumnoEjemplos.SeaSharp
             ship.Close();
             EnemyFleet.CloseAll();
             Bola.CloseAll();
+            Island.Close();
 
         }
 
